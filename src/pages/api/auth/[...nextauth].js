6f201ba7d0d473bc50582a58eed5da0009b5ed61
Md-Rizwan <rizwan@saglus.com>
2023-05-23 11:55:30 +0530

    signup setting

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -3,30 +3,30 @@ import NextAuth from "next-auth";
 // import { AmazonCognitoIdentityProvider } from "@next-auth/amazon-cognito-identity-provider";
 
 export default NextAuth({
-  providers: [
-    Providers.Credentials({
-      id: "cognito",
-      name: "Cognito",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      authorize: async (credentials) => {
-        const provider = new AmazonCognitoIdentityProvider({
-          clientId: process.env.COGNITO_CLIENT_ID,
-          clientSecret: process.env.COGNITO_CLIENT_SECRET,
-          poolId: process.env.COGNITO_POOL_ID,
-        });
+  // providers: [
+  //   Providers.Credentials({
+  //     id: "cognito",
+  //     name: "Cognito",
+  //     credentials: {
+  //       email: { label: "Email", type: "email" },
+  //       password: { label: "Password", type: "password" },
+  //     },
+  //     authorize: async (credentials) => {
+  //       const provider = new AmazonCognitoIdentityProvider({
+  //         clientId: process.env.COGNITO_CLIENT_ID,
+  //         clientSecret: process.env.COGNITO_CLIENT_SECRET,
+  //         poolId: process.env.COGNITO_POOL_ID,
+  //       });
 
-        try {
-          const user = await provider.getUser(credentials);
-          return Promise.resolve(user);
-        } catch (error) {
-          console.error(error);
-          return Promise.resolve(null);
-        }
-      },
-    }),
-  ],
+  //       try {
+  //         const user = await provider.getUser(credentials);
+  //         return Promise.resolve(user);
+  //       } catch (error) {
+  //         console.error(error);
+  //         return Promise.resolve(null);
+  //       }
+  //     },
+  //   }),
+  // ],
 });
 
